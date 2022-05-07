c5f83cd23667c6991fab1f54a86a29a85d1564bc
Carmen Tsang <carmtsang89@hotmail.com>
2022-05-07 22:14:08 +0000

    write test for findLongURL

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -1,6 +1,6 @@
 const { assert } = require('chai');
 
-const { getUserByEmail, generateRandomString } = require('../helpers.js');
+const { getUserByEmail, generateRandomString, findLongURL } = require('../helpers.js');
 
 const testUsers = {
   "userRandomID": {
@@ -15,15 +15,27 @@ const testUsers = {
   }
 };
 
+const testDatabase = {
+  'b2xVn2': {
+    longURL: "http://www.lighthouselabs.ca",
+    userID: 'userRandomID'
+  },
+  '9sm5xK': {
+    longURL: "http://www.google.com",
+    userID: 'userRandomID'
+  },
+  'as3df2': {
+    longURL: "https://www.reddit.com",
+    userID: 'randomUser'
+  }
+}
+
 describe('getUserByEmail', () => {
   it('should return a user with valid email', () => {
     const user = getUserByEmail('user@example.com', testUsers)
     const expectedUserID = 'userRandomID'
     assert.equal(user, expectedUserID);
   })
-})
-
-describe('getUserByEmail', () => {
   it('should return a undefined with invalid email', () => {
     const user = getUserByEmail('user5@example.com', testUsers)
     const expectedUserID = 'userRandomID'
@@ -31,10 +43,21 @@ describe('getUserByEmail', () => {
   })
 })
 
+describe('urlsForUser', () => {
+
+})
+
 describe('generateRandomString', () => {
   it('should create a random string with 6 values', () => {
-    
     assert.equal(generateRandomString().length, 6);
+  })
+})
+
+describe('findLongURL', () => {
+  it('should return a longURL', () => {
+    const longURL = findLongURL('as3df2', testDatabase);
+    const expectedURL = 'https://www.reddit.com';
+    assert.equal(longURL, expectedURL)
   })
 })
 
