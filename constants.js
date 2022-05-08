1789047af1ee93ef071297e2e5506c398e4031cc
Carmen Tsang <carmtsang89@hotmail.com>
2022-05-08 02:21:23 +0000

    add bcrypt to password

--- constants.js
+++ constants.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs')
+
 const urlDatabase = {
   'b2xVn2': {
     longURL: "http://www.lighthouselabs.ca",
@@ -13,12 +15,12 @@ const users = {
   'userRandomID': {
     id: 'userRandomID',
     email: 'user@example.com',
-    password: 'purple-monkey-dinosaur'
+    password: bcrypt.hashSync('test', 10)
   },
   'user2RandomID': {
     id: 'user2RandomID',
     email: 'user2@example.com',
-    password: 'dishwasher-funk'
+    password: bcrypt.hashSync('test', 10)
   }
 };
 
